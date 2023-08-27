20dc59daedfd653a65bd4190eb877609627353fe
Andra <andradwikasa@gmail.com>
2023-08-28 05:01:38 +0700

    Add register user service

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,16 @@
 import { Db } from "mongodb";
+import { UserModel } from "../models/dataModel";
 
-export async function getAllUsers(db: Db) {
+async function getAllUsers(db: Db) {
     const userCollection = db.collection('users');
     const users = await userCollection.find().toArray();
     return users
 }
+
+async function registerUser(newUser: UserModel, db: Db) {
+    const userCollection = db.collection('users');
+    const newUserRegistered = await userCollection.insertOne(newUser);
+    return newUserRegistered
+}
+
+export {getAllUsers, registerUser}
