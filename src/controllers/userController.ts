d763d6e70c8645358260fed795f73e10dbf20c1e
Andra <andradwikasa@gmail.com>
2023-08-28 05:18:34 +0700

    Hal finish register user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,22 +1,53 @@
-import { Request,Response, NextFunction } from "express";
-import jwt from "jsonwebtoken"
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import { UserModel } from "../models/dataModel";
-import { getAllUsers } from "../services/user";
-// import { validatingUser } from "../utils/userValidationResponse";
-import bcrypt from "bcrypt"
+import { getAllUsers, registerUser } from "../services/user";
+import bcrypt from "bcrypt";
 import { loggerTimestamp } from "../utils/utils";
 import { Db } from "mongodb";
 // import { secretKey } from "../middleware/jwtAuth";
 
-export async function getUsersController(req:Request, res : Response) {
-    try {
-        const users = await getAllUsers(req.db as Db)
-        res.json({
+export async function getUsersController(req: Request, res: Response) {
+	try {
+		const users = await getAllUsers(req.db as Db);
+		res.json({
+			success: true,
+			message: "Users fetched succesfully",
+			data: users,
+		});
+	} catch (error) {
+		res.json({});
+	}
+}
+async function registerUserController(req: Request, res: Response) {
+	try {
+		const { username, password, role } = req.body;
+
+		if (!username || username.trim() === "") {
+			return res.status(400).json({
+				success: false,
+				message: "Username cannot be empty or contain only whitespace.",
+			});
+		}
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const newUser = {
+			username,
+			password: hashedPassword,
+			role,
+		};
+
+        await registerUser(newUser, req.db as Db)
+
+        res.status(200).json({
             success: true,
-            message: "Users fetched succesfully",
-            data: users
+            message: "",
+            data: {
+                username: username,
+                role: role
+            }
+
         })
-    } catch (error) {
-        res.json({})
-    }
+
+	} catch (error) {}
 }
