c33c7adc1720b9cbbe0400b9c6a8d5cf9afb465c
Andra <andradwikasa@gmail.com>
2023-08-29 23:44:12 +0700

    Add delete, find , add school service

--- src/services/school.ts
+++ src/services/school.ts
@@ -2,8 +2,25 @@ import { Db, ObjectId } from "mongodb";
 import { UserModel } from "../models/dataModel";
 
 export async function getAllSchool(db: Db) {
-	const schoolCollection = db.collection("school");
+	const schoolCollection = db.collection("schools");
 	const schools = await schoolCollection.find().toArray();
 	return schools;
 }
 
+export async function addSchool(newSchool: UserModel, db: Db) {
+	const schoolCollection = db.collection("school");
+	const newSchoolAdded = await schoolCollection.insertOne(newSchool);
+	return newSchoolAdded;
+}
+
+export async function getSchoolById(id: string, db: Db) {
+	const schoolCollection = db.collection("school");
+	const schoolResult = await schoolCollection.findOne({ _id: new ObjectId(id) });
+	return schoolResult;
+}
+
+export async function deletSchoolById(id: string, db: Db) {
+	const schoolCollection = db.collection("school");
+	const schoolResult = await schoolCollection.deleteOne({ _id: new ObjectId(id) });
+	return schoolResult;
+}
