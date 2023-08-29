20c975690ae452d74fc68342d4a3aadce7fa9ae0
Andra <andradwikasa@gmail.com>
2023-08-29 20:00:02 +0700

    Refactor openapi yaml

--- src/services/school.ts
+++ src/services/school.ts
@@ -1,8 +1,9 @@
 import { Db, ObjectId } from "mongodb";
 import { UserModel } from "../models/dataModel";
 
-async function getAllSchool(db: Db) {
+export async function getAllSchool(db: Db) {
 	const schoolCollection = db.collection("school");
 	const schools = await schoolCollection.find().toArray();
 	return schools;
 }
+
