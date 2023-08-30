5ee143dd45442ba0d1c292fcbfa881df743d7ed3
Andra <andradwikasa@gmail.com>
2023-08-30 08:43:24 +0700

    Add autehntication middleware

--- dist/middlewares/auth.js
+++ dist/middlewares/auth.js
@@ -1,1 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const envInit_1 = require("../configs/envInit");
+const utils_1 = require("../utils/utils");
+function authenticationMiddleware(req, res, next) {
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: "Token not found" });
+    }
+    try {
+        const payload = jsonwebtoken_1.default.verify(token, envInit_1.secretKey);
+        req.role = payload.role;
+        next();
+    }
+    catch (error) {
+        (0, utils_1.loggerTimestamp)(`Authentication error: ${error}`);
+        return res.status(401).json({ message: "Token not valid" });
+    }
+}
