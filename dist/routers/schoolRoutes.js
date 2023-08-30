1515dc9aba03fa3ee1fa44f2406e3de416b1daf2
Andra <andradwikasa@gmail.com>
2023-08-31 00:57:12 +0700

    Add authorization on school routes

--- dist/routers/schoolRoutes.js
+++ dist/routers/schoolRoutes.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const schoolController_1 = require("../controllers/schoolController");
+const auth_1 = require("../middlewares/auth");
 const schoolRouter = (0, express_1.Router)();
-schoolRouter.get('/schools', schoolController_1.getschoolsController);
-schoolRouter.post('/schools', schoolController_1.addSchoolController);
-schoolRouter.delete('/schools/:schoolId', schoolController_1.deleteSchoolController);
-schoolRouter.patch('/schools/:schoolId', schoolController_1.updatetSchoolController);
+schoolRouter.get('/schools', (0, auth_1.authorizationMiddleware)(["admin", "moderator", "user"]), schoolController_1.getschoolsController);
+schoolRouter.post('/schools', (0, auth_1.authorizationMiddleware)(["admin", "moderator", "user"]), schoolController_1.addSchoolController);
+schoolRouter.delete('/schools/:schoolId', (0, auth_1.authorizationMiddleware)(["admin", "moderator"]), schoolController_1.deleteSchoolController);
+schoolRouter.patch('/schools/:schoolId', (0, auth_1.authorizationMiddleware)(["admin", "moderator"]), schoolController_1.updatetSchoolController);
 // router.post('/login', loginUser)
 exports.default = schoolRouter;
--- src/routers/schoolRoutes.ts
+++ src/routers/schoolRoutes.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { getschoolsController, addSchoolController, deleteSchoolController, updatetSchoolController,  } from "../controllers/schoolController";
+import { authorizationMiddleware } from "../middlewares/auth";
 
 const schoolRouter = Router()
 
 
-schoolRouter.get('/schools', getschoolsController);
+schoolRouter.get('/schools', authorizationMiddleware(["admin", "moderator", "user"]), getschoolsController);
 
-schoolRouter.post('/schools', addSchoolController);
+schoolRouter.post('/schools', authorizationMiddleware(["admin", "moderator", "user"]), addSchoolController);
 
-schoolRouter.delete('/schools/:schoolId', deleteSchoolController )
+schoolRouter.delete('/schools/:schoolId', authorizationMiddleware(["admin", "moderator"]),deleteSchoolController )
 
-schoolRouter.patch('/schools/:schoolId', updatetSchoolController)
+schoolRouter.patch('/schools/:schoolId', authorizationMiddleware(["admin", "moderator"]), updatetSchoolController)
 
 
 
