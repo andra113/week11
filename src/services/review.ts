2cebad238761c6b405cbf7dd5f35202b3a321281
Andra <andradwikasa@gmail.com>
2023-08-31 02:54:59 +0700

    Add authorization on school routes

--- src/services/review.ts
+++ src/services/review.ts
@@ -12,3 +12,29 @@ export async function addReview(newReview: ReviewModel, db: Db) {
 	const newReviewAdded = await reviewCollection.insertOne(newReview);
 	return newReview;
 }
+
+export async function getReviewById(id: string, db: Db) {
+	const reviewCollection = db.collection("review");
+	const reviewResult = await reviewCollection.findOne({ _id: new ObjectId(id) });
+	return reviewResult;
+}
+
+export async function deletReviewById(id: string, db: Db) {
+	const reviewCollection = db.collection("reviews");
+	const reviewResult = await reviewCollection.findOneAndDelete({ _id: new ObjectId(id) });
+	return reviewResult;
+}
+
+export async function updatetReviewById(id: string, updateField: any, db: Db) {
+	const reviewCollection = db.collection("reviews");
+	const setField: any = {};
+	for (const key in updateField) {
+		setField[key] = updateField[key];
+	}
+	const reviewResult = await reviewCollection.updateOne({ _id: new ObjectId(id) },
+		{
+			$set: setField
+		});
+	return reviewResult;
+}
+
